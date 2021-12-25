cac67564e6b9d12054abc9610934e8920a95b9d9
ayoub <ayoubizourane4@gmail.com>
2021-12-25 21:42:36 +0100

    covie app update

---
+++ app/src/main/java/com/example/dashboard/UserData.java
@@ -0,0 +1,12 @@
+package com.example.dashboard;
+
+public class UserData {
+    public String nom,prenom,EmailId;
+    public UserData(){
+    }
+    public UserData(String nom, String prenom,String EmailId) {
+        nom = this.nom;
+        prenom = this.prenom;
+        EmailId=this.EmailId;
+    }
+}
